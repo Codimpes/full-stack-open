f6b594cdb7ccd66e52a219b3dce1bc08843dfded
Jeyson Umaña <jeyson.umana@esinergia.co>
2025-03-08 19:18:46 -0500

    Exercise 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -18,7 +18,7 @@ const Content = (props) => {
   return (
     <div>
       {props.content.map((part) => {
-        return <Part name={part.name} amount={part.amount} />;
+        return <Part name={part.name} amount={part.exercises} />;
       })}
     </div>
   );
@@ -35,18 +35,21 @@ const Footer = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-  const names = [part1, part2, part3];
-  const amounts = [exercises1, exercises2, exercises3];
-  const content = names.map((name, index) => ({
-    name: name,
-    amount: amounts[index],
-  }));
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
+  const content = [part1, part2, part3];
+  const amounts = [part1.exercises, part2.exercises, part3.exercises];
+
   return (
     <div>
       <Header course={course} />
