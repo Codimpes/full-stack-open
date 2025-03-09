c211b2481dc80184543e14867110c251e7c694c0
Jeyson Umaña <jeyson.umana@esinergia.co>
2025-03-09 11:17:28 -0500

    Exercise 2.2, 2.3

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -18,8 +18,8 @@ const Course = (props) => {
   return (
     <div>
       <Header course={props.course} />
-      {props.course.parts.map((part, index) => {
-        return <Part key={index} name={part.name} exercises={part.exercises} />;
+      {props.course.parts.map((part) => {
+        return <Part key={part.id} name={part.name} exercises={part.exercises} />;
       })}
       <Footer course={props.course} />
     </div>
@@ -31,26 +31,32 @@ const Footer = (props) => {
   const total = amounts.reduce((acc, num) => acc + num, 0);
   return (
     <>
-      <p>Number of exercises {total}</p>
+      <p>
+        <strong>Total of {total} exercises</strong>
+      </p>
     </>
   );
 };
 
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
